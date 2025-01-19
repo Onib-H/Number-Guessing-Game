d481da08ec217e13c7e5757e33d7df69adb88d9f
Harold Aldovino <haroldaldovino@gmail.com>
2025-01-19 16:19:39 +0800

    Optimized it

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const prompt = require("prompt-sync")();
 
 function numberGuessingGame() {
-  console.log("Welcome to Number Guessing Game");
+  console.log("Welcome to Number Guessing Game.");
   console.log("I've picked number between 1 and 100. Can you guess it?");
+  console.log("You have a 10 attempts.");
 
   const random = Math.trunc(Math.random() * 100 + 1);
   let attempt = 0;
   let guessCorrectly = false;
+  const maxAttempt = 10;
 
-  while (!guessCorrectly) {
+  while (!guessCorrectly && attempt < maxAttempt) {
     let guess = parseInt(prompt("Enter your guess(1-100):"));
 
     if (isNaN(guess) || guess < 1 || guess > 100) {
@@ -29,8 +31,14 @@ function numberGuessingGame() {
     } else {
       console.log("Too Low");
     }
-  }
 
+    if (!guessCorrectly && attempt < maxAttempt) {
+      console.log(`You have ${maxAttempt - attempt} attempts remaining.`);
+    }
+  }
+  if (!guessCorrectly) {
+    console.log(`Game over! The correct number was ${random}.`);
+  }
   console.log("Thank you for playing!");
 }
 numberGuessingGame();
