7a104aedb4357817f8a7d285dc80a5fd1917bc50
Harold Aldovino <haroldaldovino@gmail.com>
2025-01-19 16:03:50 +0800

    Make it in recurssion

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 const prompt = require("prompt-sync")();
 
-console.log("Welcome to Number Guessing Game");
-console.log("I've picked number between 1 and 100. Can you guess it?");
+function numberGuessingGame() {
+  console.log("Welcome to Number Guessing Game");
+  console.log("I've picked number between 1 and 100. Can you guess it?");
 
-const random = Math.trunc(Math.random() * 100 + 1);
-let attempt = 0,
-  choice = 0;
+  const random = Math.trunc(Math.random() * 100 + 1);
+  let attempt = 0,
+    choice = 0;
+  let guessCorrectly = false;
 
-while (choice != random) {
-  choice = prompt("Enter your guess(1-100):");
-  choice = Number(choice);
-  attempt++;
+  function guessingGame() {
+    const guess = parseInt(prompt("Enter your guess(1-100):"));
 
-  if (choice > random) {
-    console.log("Too high");
-  } else if (choice < random) {
-    console.log("Too low");
+    if (isNaN(guess) || guess < 1 || guess > 100) {
+      console.log("Please enter a valid number between 1 and 100");
+      guessNumber();
+      return;
+    }
+
+    attempt++;
+
+    if (guess === random) {
+      let attempts = attempt === 1 ? "attempt" : "attempts";
+      console.log(
+        `Congratulation! You guess the number in ${attempt} ${attempts}`
+      );
+      guessCorrectly = true;
+    } else if (guess > random) {
+      console.log("Too High");
+      guessingGame();
+    } else {
+      console.log("Too Low");
+      guessingGame();
+    }
+  }
+
+  guessingGame();
+
+  if (guessCorrectly) {
+    console.log("Thank you for playing!");
   }
 }
-if (attempt == 1) {
-  console.log(`Congratulations! You guess the number in ${attempt} attempt`);
-} else {
-  console.log(`You guess the number in ${attempt} attempts`);
-}
-
-console.log("Thanks for playing!");
+numberGuessingGame();
